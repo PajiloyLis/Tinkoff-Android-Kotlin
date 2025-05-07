15e7e00455fda7f92eb9943f319958438a267cea
Bugakov Ivan <bugakov0601@yandex.ru>
2025-05-07 09:25:52 +0300

    second task

---
+++ homework2/src/DataClass.kt
@@ -0,0 +1,44 @@
+
+/**
+ * ДЗ:
+ * В уроке про дата классы мы рассматривали, как можно вывести в терминал имя первого учителя из списка.
+ * Давайте чуть усложним задачу и напишем фильтрацию преподавателей по опыту.
+ *
+ * У нас есть список учителей, нужно вывести список имен учителей, опыт которых больше 5 лет.
+ * А если такого учителя нет, то нужно вывести текст: "Учитель с таким опытом не найден"
+ *
+ * Для выполнения задачи понадобится методы filter и map из Kotlin.
+ * Примеры использования можно изучить в официальной документации Kotlin:
+ * 1. https://kotlinlang.ru/docs/collection-elements.html
+ * 2. https://kotlinlang.ru/docs/map-operations.html
+ */
+fun main() {
+
+    val languageList = listOf(
+        Language("Kotlin"),
+        Language("Java"),
+    )
+
+    val teacherList = listOf(
+        Teacher("Николай", 37, 2, languageList),
+        Teacher("Кирилл", 12, 3, languageList),
+        Teacher("Анастасия", 7, 5, languageList),
+        Teacher("Евгений", 2, 1, languageList)
+    )
+
+    val experiencedTeacher = getExperiencedTeacher(experience = 5, teacherList = teacherList)
+
+    if (experiencedTeacher.isNotEmpty()) {
+        println(experiencedTeacher)
+    } else {
+        println("Учитель с таким опытом не найден")
+    }
+}
+
+/**
+ * Метод поиска опытного учителя из списка
+ */
+private fun getExperiencedTeacher(experience: Int, teacherList: List<Teacher>): List<String> {
+    val mapped = teacherList.map { teacher -> teacher.name to teacher.progExperience }
+    return teacherList.filter { teacher: Teacher ->  teacher.progExperience>experience}.map{it.name}
+}
---
+++ homework2/src/Teacher.kt
@@ -0,0 +1,6 @@
+data class Teacher(
+    val name: String,
+    val age: Int,
+    val progExperience: Int,
+    val languages: List<Language>
+)
