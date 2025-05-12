22f75e3085595c0a8e502d56e7c823789a875859
Bugakov Ivan <bugakov0601@yandex.ru>
2025-05-12 17:24:33 +0300

    simple suspend

---
+++ example_coroutine/app/src/main/java/com/example/example_coroutine/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.example_coroutine
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import android.util.Log
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlin.math.log
+import kotlin.system.measureTimeMillis
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        CoroutineScope(Dispatchers.IO + CoroutineName("KEK")).launch {
+            val time = measureTimeMillis {
+                val valOne = callOne()
+                val valTwo = callTwo()
+                Log.d("TAG", valOne)
+                Log.d("TAG", valTwo)
+            }
+            Log.d("TAG", "Time ${time}")
+        }
+    }
+
+    private suspend fun callOne(): String{
+        delay(1000)
+        return "callOne"
+    }
+
+    private suspend fun callTwo(): String{
+        delay(3000)
+        return "callTwo"
+    }
+}
