8d7550400914cbd671314b95cc2fe10f6ef74384
Bugakov Ivan <bugakov0601@yandex.ru>
2025-05-12 18:12:19 +0300

    simple collect

---
+++ example_flow/app/src/main/java/com/example/example_flow/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.example_flow
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private val flow: Flow<String> = flow {
+        delay(1000)
+        emit("Hello")
+        delay(1000)
+        emit("kotlin")
+        delay(1000)
+        emit("flows")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch{
+            flow.collect{data ->
+                delay(2000)
+                Log.d("TAG", data)}
+        }
+
+    }
+}
